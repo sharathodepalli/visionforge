e3227a9c83613105c7850bed58458333c84ffa6f
sharathodepalli <sharathodepalli@gmail.com>
2024-12-08 23:36:40 -0500

    Updated Navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import '../styles/Navbar.css';
 
 const Navbar = () => {
@@ -11,24 +10,40 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`navbar ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
+    <nav className="navbar">
       <div className="navbar-container">
+        {/* Logo */}
         <div className="logo">
-          <Link to="/">VisionForge</Link>
+          <Link to="/">VISIONFORGE</Link>
         </div>
+
+        {/* Menu Toggle for Mobile */}
         <div className="menu-toggle" onClick={toggleMobileMenu}>
-          <span className="hamburger"></span>
+          <div className={`hamburger ${isMobileMenuOpen ? 'open' : ''}`}></div>
         </div>
+
+        {/* Navigation Links */}
         <ul className={`nav-links ${isMobileMenuOpen ? 'nav-links-mobile' : ''}`}>
-          <li><Link to="/" onClick={() => setIsMobileMenuOpen(false)}>Home</Link></li>
-          <li><Link to="/about-us" onClick={() => setIsMobileMenuOpen(false)}>About Us</Link></li>
-          <li><Link to="/projects" onClick={() => setIsMobileMenuOpen(false)}>Projects</Link></li>
-          <li><Link to="/join-team" onClick={() => setIsMobileMenuOpen(false)}>Join Team</Link></li>
-          <li><Link to="/contact-us" onClick={() => setIsMobileMenuOpen(false)}>Contact Us</Link></li>
+          <li>
+            <Link to="/" onClick={() => setIsMobileMenuOpen(false)}>Home</Link>
+          </li>
+          <li>
+            <Link to="/about-us" onClick={() => setIsMobileMenuOpen(false)}>About Us</Link>
+          </li>
+          <li>
+            <Link to="/projects" onClick={() => setIsMobileMenuOpen(false)}>Projects</Link>
+          </li>
+          <li>
+            <Link to="/join-team" onClick={() => setIsMobileMenuOpen(false)}>Join Team</Link>
+          </li>
+          <li>
+            <Link to="/contact-us" onClick={() => setIsMobileMenuOpen(false)}>Contact Us</Link>
+          </li>
         </ul>
+
+        {/* Call to Action Button */}
         <div className="cta-button">
           <Link to="/join-team" onClick={() => setIsMobileMenuOpen(false)}>Join Us Now</Link>
-          
         </div>
       </div>
     </nav>
